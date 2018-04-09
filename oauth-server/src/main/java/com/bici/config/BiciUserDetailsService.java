a76cbd81fc3af240785e5bd57db68c53405e5832
keluosi <keluosi@bicitech.cn>
2018-04-09 17:50:44 +0800

    自定义授权

--- oauth-server/src/main/java/com/bici/config/BiciUserDetailsService.java
+++ oauth-server/src/main/java/com/bici/config/BiciUserDetailsService.java
@@ -1,11 +1,15 @@
 package com.bici.config;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description:
@@ -17,6 +21,9 @@ import org.springframework.stereotype.Component;
 public class BiciUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User(username, "{noop}123456", AuthorityUtils.commaSeparatedStringToAuthorityList("USER,ADMIN"));
+        List<GrantedAuthority> grantedAuthorityList = new ArrayList<>();
+        grantedAuthorityList.add(new BiciGrantedAuthority("bici_role1", "bici_menu1"));
+        grantedAuthorityList.add(new BiciGrantedAuthority("bici_role2", "bici_menu2"));
+        return new User(username, "{noop}123456", grantedAuthorityList);
     }
 }
